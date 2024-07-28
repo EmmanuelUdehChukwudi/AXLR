c6df6f4d07e406c63b65df43671e7b0c045aff59
Emmanuelprime <emmanuelprime2@gmail.com>
2024-07-28 19:47:20 +0100

    added ros2 diff drive control

--- src/axlr_controller/launch/controller.launch.py
+++ src/axlr_controller/launch/controller.launch.py
@@ -1,10 +1,15 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument,GroupAction
 from launch.substitutions import LaunchConfiguration
-def generate_launch_description():
+from launch.conditions import UnlessCondition, IfCondition
 
+def generate_launch_description():
+    use_custom_axlr_controller_arg = DeclareLaunchArgument(
+        "use_custom_axlr_controller",
+        default_value = "True"
+    )
     wheel_radius_arg = DeclareLaunchArgument(
         "wheel_radius",
         default_value = "0.042"
@@ -15,6 +20,7 @@ def generate_launch_description():
     )
     wheel_radius = LaunchConfiguration("wheel_radius")
     wheel_separation = LaunchConfiguration("wheel_separation")
+    use_custom_axlr_controller = LaunchConfiguration("use_custom_axlr_controller")
     
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -26,27 +32,44 @@ def generate_launch_description():
         ],
     )
     
-    axlr_sim_custom_controller = Node(
-        package="axlr_controller",
-        executable="axlr_simple.py",
-        parameters=[{"wheel_radius": wheel_radius, "wheel_separation": wheel_separation}],
+    axlr_custom_controller = GroupAction(
+        condition=IfCondition(use_custom_axlr_controller),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["axlr_controller", 
+                           "--controller-manager", 
+                           "/controller_manager"
+                ]
+            ),
+            Node(
+                package="axlr_controller",
+                executable="axlr_simple.py",
+                parameters=[
+                    {"wheel_radius": wheel_radius,
+                     "wheel_separation": wheel_separation}],
+            ),
+        ]
     )
 
-    simple_controller = Node(
+    simulated_axlr_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["axlr_controller",
+        arguments=["axlr_ros_controller",
                 "--controller-manager",
                 "/controller_manager"
-        ]
+        ],
+        condition=UnlessCondition(use_custom_axlr_controller),
     )
 
     return LaunchDescription(
         [
             wheel_radius_arg,
             wheel_separation_arg,
-            axlr_sim_custom_controller,
+            use_custom_axlr_controller_arg,
+            axlr_custom_controller,
             joint_state_broadcaster_spawner,
-            simple_controller,
+            simulated_axlr_controller,
         ]
     )
